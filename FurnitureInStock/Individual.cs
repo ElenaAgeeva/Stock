3bdb5c890f1a0d7aa7561bbe58e57d205ad9d83a
ElenaAgeeva <eageeva14@yandex.ru>
2016-02-01 23:23:20 +0300

    AddSCommonAndPCommon

--- FurnitureInStock/Individual.cs
+++ FurnitureInStock/Individual.cs
@@ -20,6 +20,10 @@ namespace FurnitureInStock
 
         private List<AdditionalInformation> additionalInformationAboutIndividual;
 
+        private double SCommon;
+
+        private double PCommon;
+
         public Individual(int _numberOfFurniture, int _maxCountOfOneKindOfFurniture, List<AdditionalInformation> _additionalInformationAboutIndividual)
         {
             numberOfFurniture = _numberOfFurniture;
@@ -28,6 +32,27 @@ namespace FurnitureInStock
             genotypeOfIndividual = new Genotype(numberOfFurniture, maxCountOfOneKindOfFurniture);
             individual = genotypeOfIndividual.FormRandomGenotype();
             phenotypeOfIndividual = genotypeOfIndividual.GenotypeToPhenotype();
+            CountPCommon();
+            CountSCommon();
+        }
+
+        private void CountPCommon()
+        {
+            for(int i=0; i<individual.Count; i++)
+            {
+                PCommon += individual[i] * (additionalInformationAboutIndividual[i].getTheProbabilityOfSellingTheFurniture() *
+                    additionalInformationAboutIndividual[i].getProfitsFromTheSaleOf() - additionalInformationAboutIndividual[i].getStorageCosts());
+            }
+        }
+
+        private void CountSCommon()
+        {
+            for (int i = 0; i < individual.Count; i++)
+            {
+                SCommon += individual[i] * (additionalInformationAboutIndividual[i].getConsumptionDueToTheLackOfStockss()+
+                    additionalInformationAboutIndividual[i].getAccommodationCosts() + additionalInformationAboutIndividual[i].getMaintenanceCosts()+
+                    additionalInformationAboutIndividual[i].getManufacturingCosts());
+            }
         }
     }
 }
