b7d0372c6d0e5c0e983bb182f82d1f155fcd6b91
ElenaAgeeva <eageeva14@yandex.ru>
2016-02-04 00:43:28 +0300

    Частично добавила визаализацию

--- FurnitureInStock/Form1.Designer.cs
+++ FurnitureInStock/Form1.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,6 +44,8 @@
             this.MainOutputData = new System.Windows.Forms.DataGridView();
             this.OtherOutputData = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.MainOutputData)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -124,6 +127,9 @@
             // MainOutputData
             // 
             this.MainOutputData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainOutputData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3});
             this.MainOutputData.Location = new System.Drawing.Point(38, 301);
             this.MainOutputData.Name = "MainOutputData";
             this.MainOutputData.RowTemplate.Height = 24;
@@ -141,19 +147,36 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(481, 28);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series2.Legend = "Legend1";
+            series2.Name = "Ген. Алг";
+            series2.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(434, 249);
             this.chart1.TabIndex = 11;
             this.chart1.Text = "chart1";
+            title2.Name = "Title1";
+            title2.Text = "Критериальное пространство";
+            this.chart1.Titles.Add(title2);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Si";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Pi";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             // 
             // Form1
             // 
@@ -195,6 +218,8 @@
         private System.Windows.Forms.DataGridView MainOutputData;
         private System.Windows.Forms.TextBox OtherOutputData;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- FurnitureInStock/Form1.cs
+++ FurnitureInStock/Form1.cs
@@ -15,11 +15,30 @@ namespace FurnitureInStock
         public Form1()
         {
             InitializeComponent();
+            this.MainOutputData.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgvUserDetails_RowPostPaint);
+            this.MainOutputData.TopLeftHeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.MainOutputData.TopLeftHeaderCell.Value = "i";
+        }
+
+        private void dgvUserDetails_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(MainOutputData.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                var centerFormat = new StringFormat()
+                {
+                    // right alignment might actually make more sense for numbers
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Center
+                };
+
+                var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, (sender as DataGridView).RowHeadersWidth, e.RowBounds.Height);
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, b, headerBounds, centerFormat);
+            }
         }
 
         private void SolveWithGeneticAlgorithm_Click(object sender, EventArgs e)
         {
-            Population population = new Population(5, 7, 2,15,1000,10);
+            Population population = new Population(20, 7, 2,15,1000,100);
             population.FormInitialPopulation();
             List<Individual> initialPopulation = population.getInitialPopulation();
             initialPopulation = population.FindFitnessAssessment(initialPopulation);
@@ -27,6 +46,12 @@ namespace FurnitureInStock
             OtherOutputData.Text += "P = " + population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon().ToString()+"\r\n";
             OtherOutputData.Text += "X = " + string.Join(", ", population.getCommonNonDominatedOptions().FirstOrDefault().getIndividual()) + "\r\n";
             OtherOutputData.Text += "========================= 0\r\n";
+            chart1.Series[0].Points.AddXY(population.getCommonNonDominatedOptions().FirstOrDefault().getSCommon(),
+                population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon());
+            MainOutputData.Rows.Add();
+            MainOutputData.Rows[0].Cells[0].Value = population.getCommonNonDominatedOptions().FirstOrDefault().getSCommon().ToString();
+            MainOutputData.Rows[0].Cells[1].Value = population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon().ToString();
+
             int iteration = 1;
             do
             {
@@ -39,6 +64,11 @@ namespace FurnitureInStock
                 OtherOutputData.Text += "P = " + population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon().ToString() + "\r\n";
                 OtherOutputData.Text += "X = " + string.Join(", ", population.getCommonNonDominatedOptions().FirstOrDefault().getIndividual()) + "\r\n";
                 OtherOutputData.Text += "========================= "+iteration.ToString()+" \r\n";
+                chart1.Series[0].Points.AddXY(population.getCommonNonDominatedOptions().FirstOrDefault().getSCommon(),
+                population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon());
+                MainOutputData.Rows.Add();
+                MainOutputData.Rows[iteration].Cells[0].Value = population.getCommonNonDominatedOptions().FirstOrDefault().getSCommon().ToString();
+                MainOutputData.Rows[iteration].Cells[1].Value = population.getCommonNonDominatedOptions().FirstOrDefault().getPCommon().ToString();
                 iteration++;
             }
             while (population.CheckForNoImprovment());   
